51ce59c690e70b83321511a5113821052483c8a1
林培涛 <877671762@qq.com>
2026-07-22 17:17:20 +0800

    修改gateway代码

--- seckill-gateway/src/main/java/com/seckill/gateway/SeckillGatewayApplication.java
+++ seckill-gateway/src/main/java/com/seckill/gateway/SeckillGatewayApplication.java
@@ -3,9 +3,11 @@ package com.seckill.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.seckill.gateway.client")
 public class SeckillGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SeckillGatewayApplication.class, args);
